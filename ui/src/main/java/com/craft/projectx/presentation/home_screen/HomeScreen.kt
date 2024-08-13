ba68b201c901a1f1d7167d1fb85e04d3b178de22
ankitsinghCred <ankit.singh@cred.club>
2024-08-14 00:14:04 +0530

    fix: scrollable

--- ui/src/main/java/com/craft/projectx/presentation/home_screen/HomeScreen.kt
+++ ui/src/main/java/com/craft/projectx/presentation/home_screen/HomeScreen.kt
@@ -11,9 +11,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -31,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.craft.projectx.R
@@ -55,14 +58,10 @@ fun HomeScreen(
 
     Scaffold(
         content = { paddingValues ->
-            val scrollState =
-                rememberScrollState()
-
             Column(
                 modifier = modifier
-                    .scrollable(state = scrollState, orientation = Orientation.Vertical)
+                    .verticalScroll(rememberScrollState())
                     .fillMaxSize()
-                    .fillMaxWidth()
                     .padding(paddingValues)
             ) {
                 ActiveButtonCard()
@@ -271,12 +270,13 @@ fun AppSelectCard(usageData: List<UsageData>, uiCallbacks: (UiCallback) -> Unit)
                 )
             }
 
-            LazyColumn(
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .wrapContentHeight()
                     .padding(horizontal = 16.dp, vertical = 8.dp)
             ) {
-                items(usageData) { data ->
+                for (data in usageData) {
                     AppsSelectCardDetail(data, uiCallbacks)
                     Spacer(modifier = Modifier.padding(8.dp))
                 }
